56fc0b0382c961b5a136cf4a3a5dd64f84ea215f
vision-05 <t.frankel@outlook.com>
2025-10-24 15:30:28 +0100

    created cmake

---
+++ build/CMakeFiles/tfs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tfs.dir/link.d"
+  "CMakeFiles/tfs.dir/src/main.cpp.o"
+  "CMakeFiles/tfs.dir/src/main.cpp.o.d"
+  "tfs"
+  "tfs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tfs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
