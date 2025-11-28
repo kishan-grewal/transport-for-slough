a1e5297089d5847dbc0d68e624a495005d9b5f36
Han Se Kendall <hskendall2005@outlook.com>
2025-11-28 15:23:38 +0000

    Class extraction, and more labelling

--- data-collection/station_structure/retrieve_data.py
+++ data-collection/station_structure/retrieve_data.py
@@ -2,32 +2,12 @@
 ## https://techforum.tfl.gov.uk/t/stop-structure-station-and-stop-topology-api/2796
 ## https://content.tfl.gov.uk/example-api-requests.pdf
 
+from station_structure import Station_Structure
+
 import requests
-from xml.etree import ElementTree
-import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
 
-def check_is_platform(element : ElementTree.Element):
-  # Check the item serves a line before the next check
-  try:
-    next(element.iter('itdServingLine'))
-  except:
-    return False
-
-  for item in element.iter('itdServingLine'):
-    name_field = item.find("./itdOperator/name")
-    if name_field is not None and name_field.text == "London Underground":
-      break
-  else:
-    return False
-  return True
-
-def create_dataframe(data : list[dict]):
-  df = pd.DataFrame(data)
-  df['area'] = df['area'].astype(int); df['stopID'] = df['stopID'].astype(int)
-  df = df.set_index(["stopID","area"]).sort_index(level=["stopID","area"])
-  return df
 
 def get_station_ids(line: str) -> list[tuple[str, str]]:
   url = f"https://api.tfl.gov.uk/line/{line.lower()}/stoppoints"
@@ -35,77 +15,23 @@ def get_station_ids(line: str) -> list[tuple[str, str]]:
   response = requests.get(url).json()
   return [(str(s["commonName"]),str(s["naptanId"])) for s in response]
 
-def cast_cols_to_type(df : pd.DataFrame, cols: list[str], type : type):
-  for col in cols:
-    df[col] = df[col].astype(type)
-
 station_ids = get_station_ids("jubilee")
 # print("Station NAPTAN ids:")
 # print(*station_ids,sep="\n")
 # print()
 
-stop = station_ids[2]
+stop = station_ids[9]
 print(f"{stop[0]} information ({stop[1]})\n")
-url = f"https://api.tfl.gov.uk/jp_public/api10/XML_STOPSTRUCTURE_REQUEST?sSStopNr={stop[1]}&sSOnlyDF=1&sSInclSL=1&coordOutputFormat=WGS84[dd.ddddd]&app_key=78cbd517b8b34753a87ade64492c8699"
+station = Station_Structure(stop[1])
+# station = Station_Structure("9400ZZLUBNK")
+station.drop_disconnected()
 
-response = requests.get(url)
-tree = ElementTree.fromstring(response.content)
-
-# print(tree.attrib)
-platforms_list = []
-nodes_list = []
-
-for item in tree.iter('stopAreaLines'):
-  stop_area = item.find("./stopArea"); itdPoint = item.find('./stopArea/itdPoint')
-  data = (stop_area.attrib if stop_area is not None else {}) | (itdPoint.attrib if itdPoint is not None else {})
-
-  if(check_is_platform(item)):
-    platforms_list.append(data)
-  nodes_list.append(data)
-  
-nodes = create_dataframe(nodes_list); platforms = create_dataframe(platforms_list)
-cast_cols_to_type(nodes, ["x","y","level"], float)
-cast_cols_to_type(platforms, ["x","y","level"], float)
-del nodes_list, platforms_list
-
-print("Nodes:\n",nodes)
-# print("\n\nPlatforms:\n",platforms)
-
-edges_list = []
-for item in tree.iter('footpathInfo'):
-  edge = {}
-  points = item.findall("./itdPoint")
-  if len(points) != 2:
-    raise Exception(f"Invalid footpath - Must contain 2 points, not {len(points)}")
-  edge["start"] = (int(points[0].attrib["stopID"]),int(points[0].attrib["area"]))
-  edge["end"] = (int(points[1].attrib["stopID"]),int(points[1].attrib["area"]))
-  
-  info = item.findall("./footpathPartInfos/footpathPartInfo")
-  if len(info) != 1:
-    print("Warn - skipping path with multiple info fields")
-    continue
-  edge["type"] = info[0].attrib["type"]
-  edges_list.append(edge)
-
-
-edges = pd.DataFrame(edges_list)
-del edges_list
+## Visualisation
+nodes,ids, plats, lines, edges, edge_labels = station.get_nodes_edges_plot()
 
 fig = plt.figure()
-ax = plt.axes(projection="3d")
 
-tmp = nodes[(~nodes.isin(platforms)["level"]).values] # Select non-platform nodes
-ax.scatter(tmp["x"],tmp["y"],tmp["level"])
-ax.scatter(platforms["x"],platforms["y"],platforms["level"] * 0.5,c="red")
-ax.legend(["Nodes","Platforms"])
-del tmp
-
-for _, edge in edges.iterrows():
-  ax.plot([nodes.loc[edge["start"]]["x"],nodes.loc[edge["end"]]["x"]],
-          [nodes.loc[edge["start"]]["y"],nodes.loc[edge["end"]]["y"]],
-          [nodes.loc[edge["start"]]["level"]*0.5,nodes.loc[edge["end"]]["level"]*0.5],c="black",linewidth=1)
-
-
+ax = fig.add_subplot(1,3,(1,2),projection="3d")
 # Connect event to maintain locked roll
 def on_mouse_move(event):
     # Get current view
@@ -115,15 +41,21 @@ def on_mouse_move(event):
     fig.canvas.draw_idle()
 fig.canvas.mpl_connect('motion_notify_event', on_mouse_move)
 
-plt.show()
 
-g = nx.MultiDiGraph()
-for ind in nodes.index:
-  g.add_node(ind, attr=nodes.loc[ind])
-for _, edge in edges.iterrows():
-  g.add_edge(edge["start"],edge["end"])
-g.remove_nodes_from([node for node, degree in g.degree if degree == 0]) # type: ignore
+ax.scatter(nodes[0,:],nodes[1,:],nodes[2,:])
+ax.scatter(plats[0,:],plats[1,:],plats[2,:],c="red")
+# for node,id in enumerate(ids):
+#    ax.text(nodes[0,node],nodes[1,node],nodes[2,node]+0.05,str(id),fontsize=5) # type: ignore
+for platform,id in enumerate(lines):
+   ax.text(plats[0,platform],plats[1,platform],plats[2,platform]-0.1,str(id),fontsize=7) #type: ignore
+ax.legend(["Nodes","Platforms"])
+ax.add_collection3d(edges) # type: ignore
+for pos, item in edge_labels.items():
+   ax.text(pos[0], pos[1], pos[2], str(item),fontsize=5) #type: ignore
 
-fig = plt.figure(1, dpi=50)
-nx.draw_spring(g,node_size=100)
+ax.grid(False); ax.set_xticks([]); ax.set_yticks([]); ax.set_zticks([]) # type: ignore
+
+ax2 = fig.add_subplot(1,3,3)
+g,c = station.create_nx_graph()
+nx.draw_spring(g,ax=ax2,node_size=100,node_color=c)
 plt.show()
---
+++ data-collection/station_structure/station_structure.py
@@ -0,0 +1,141 @@
+import requests
+from xml.etree import ElementTree
+import pandas as pd
+import numpy as np
+import networkx as nx
+
+from mpl_toolkits.mplot3d.art3d import Line3DCollection
+
+class Station_Structure:
+  def __init__(self, stop_id : str, ignore_busstops : bool = True):
+    url = f"https://api.tfl.gov.uk/jp_public/api10/XML_STOPSTRUCTURE_REQUEST?sSStopNr={stop_id}&sSOnlyDF=1&sSInclSL=1&coordOutputFormat=WGS84[dd.ddddd]&app_key=78cbd517b8b34753a87ade64492c8699"
+
+    response = requests.get(url)
+    tree = ElementTree.fromstring(response.content)
+
+    platforms_list = []
+    nodes_list = []
+
+    ## Build nodes
+    for item in tree.iter('stopAreaLines'):
+      stop_area = item.find("./stopArea"); itdPoint = item.find('./stopArea/itdPoint')
+      data = (stop_area.attrib if stop_area is not None else {}) | (itdPoint.attrib if itdPoint is not None else {})
+
+      if(Station_Structure.__check_is_platform(item)):
+        platforms_list.append(data)
+
+      elif ignore_busstops and item.find("./itdServingLines/itdServingLine/itdNoTrain") is not None:
+        # print("Ignored bus stop node")
+        continue
+      nodes_list.append(data)
+      
+    self.nodes = Station_Structure.__create_dataframe(nodes_list); self.platforms = Station_Structure.__create_dataframe(platforms_list)
+
+    ## Build edges
+    edges_list = []
+    for item in tree.iter('footpathInfo'):
+      edge = {}
+      points = item.findall("./itdPoint")
+      if len(points) != 2:
+        raise Exception(f"Invalid footpath - Must contain 2 points, not {len(points)}")
+      edge["start"] = (int(points[0].attrib["stopID"]),int(points[0].attrib["area"]))
+      edge["end"] = (int(points[1].attrib["stopID"]),int(points[1].attrib["area"]))
+
+      if not (edge["start"] in self.nodes.index and edge["end"] in self.nodes.index):
+        continue
+      
+      info = item.findall("./footpathPartInfos/footpathPartInfo")
+      if len(info) != 1:
+        print("Warn - skipping path with multiple info fields")
+        continue
+      edge["type"] = info[0].attrib["type"]
+      edge["duration"] = item.attrib["duration"]
+      edge["distance"] = item.attrib["distance"]
+      # edge["attributes"] = info[0].find("./attributes").attrib # type: ignore
+      edges_list.append(edge)
+
+
+    self.edges = pd.DataFrame(edges_list)
+
+  def drop_disconnected(self):
+    indx_copy = self.nodes.index.copy()
+    for node_idx in indx_copy:
+      if node_idx not in self.edges["start"].values.tolist() and node_idx not in self.edges["end"].values.tolist():
+        self.nodes.drop(node_idx,axis=0,inplace=True)
+    pass
+
+  def create_nx_graph(self) -> tuple[nx.MultiDiGraph,list]:
+    g = nx.MultiDiGraph()
+    colours = []
+    for ind in self.nodes.index:
+      g.add_node(ind, attr=self.nodes.loc[ind])
+      colours.append("red" if ind in self.platforms.index else "blue")
+    for _, edge in self.edges.iterrows():
+      g.add_edge(edge["start"],edge["end"])
+    g.remove_nodes_from([node for node, degree in g.degree if degree == 0]) # type: ignore
+
+    return g, colours
+
+  def get_nodes_edges_plot(self):
+    tmp = self.nodes[(~self.nodes.isin(self.platforms)["level"]).values] # Select non-platform nodes
+    nodes_pos = np.array([tmp["x"],tmp["y"],tmp["level"] * 0.5])
+    nodes_ids = np.array(tmp.index)
+    platforms_pos = np.array([self.platforms["x"],self.platforms["y"],self.platforms["level"] * 0.5])
+    platforms_ids = self.platforms["areaName"].values
+
+    start = self.nodes.loc[self.edges["start"]]
+    end = self.nodes.loc[self.edges["end"]]
+
+    edge_data = {}
+    for i in self.edges.index:
+      edge = self.edges.loc[i]
+      if (edge["start"],edge["end"]) in edge_data:
+        edge_data[(edge["start"],edge["end"])].append(edge["type"])
+      elif (edge["end"],edge["start"]) in edge_data:
+        if edge["type"] not in edge_data[(edge["end"],edge["start"])]:
+          edge_data[(edge["end"],edge["start"])].append(edge["type"])
+      else:
+        edge_data[(edge["start"],edge["end"])] = [edge["type"]]
+
+    def calc_pos(start, end):
+      return ((start["x"] + end["x"]) / 2, (start["y"] + end["y"]) / 2, (start["level"] + end["level"]) / 4)
+    edge_labels = {calc_pos(self.nodes.loc(axis=0)[key[0]],self.nodes.loc(axis=0)[key[1]]) : value for key,value in edge_data.items()}
+
+    # Lines
+    start = np.array([start["x"].values, start["y"].values, (start["level"] * 0.5).values]).T.reshape(-1,1,3)
+    end = np.array([end["x"].values, end["y"].values, (end["level"] * 0.5).values]).T.reshape(-1,1,3)
+    segments = np.concatenate([start, end],axis=1)
+    edge_quiver = Line3DCollection(segments, colors="k", linewidths=0.5)
+    
+
+    return nodes_pos,nodes_ids, platforms_pos,platforms_ids, edge_quiver, edge_labels
+
+  @staticmethod
+  def __check_is_platform(element : ElementTree.Element):
+  # Check the item serves a line before the next check
+    try:
+      next(element.iter('itdServingLine'))
+    except:
+      return False
+
+    for item in element.iter('itdServingLine'):
+      name_field = item.find("./itdOperator/name")
+      ## Second check is since sometimes it's counted as a different service (e.g. at canary wharf)
+      if name_field is not None and (name_field.text == "London Underground" or name_field.text == "Elizabeth line"):
+        break
+    else:
+      return False
+    return True
+
+  @staticmethod
+  def __create_dataframe(data : list[dict]):
+    df = pd.DataFrame(data)
+    df['area'] = df['area'].astype(int); df['stopID'] = df['stopID'].astype(int)
+    df = df.set_index(["stopID","area"]).sort_index(level=["stopID","area"])
+    Station_Structure.__cast_cols_to_type(df, ["x","y","level"], float)
+    return df
+
+  @staticmethod
+  def __cast_cols_to_type(df : pd.DataFrame, cols: list[str], type : type):
+    for col in cols:
+      df[col] = df[col].astype(type)
